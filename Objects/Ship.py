ba744abf2f7a3b39cdcb287bf6f1bc83806eaaac
Bo Wang <bowang355@gmail.com>
2026-06-02 09:26:15 +1000

    Add Laser

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,4 +1,5 @@
 from GameFrame import RoomObject, Globals
+from Objects.Laser import Laser
 import pygame
 
 class Ship(RoomObject):
@@ -14,10 +15,12 @@ class Ship(RoomObject):
         
         # set image
         image = self.load_image("Ship.png")
-        self.set_image(image,100,100)
+        self.set_image(image,75,75)
 
         # register events
         self.handle_key_events = True
+
+        self.can_shoot = True
         
     def key_pressed(self, key):
         """
@@ -32,6 +35,8 @@ class Ship(RoomObject):
             self.x -= 7.5
         elif key[pygame.K_d]:
             self.x += 7.5
+        if key[pygame.K_SPACE]:
+            self.shoot_laser()
     
     def keep_in_room(self):
         """
@@ -51,3 +56,20 @@ class Ship(RoomObject):
         Determine what happens to the Ship on each click of the game clock
         """
         self.keep_in_room()
+
+    def shoot_laser(self):
+        """
+        Shoots a laser from the ship
+        """
+        new_laser = Laser(self.room, 
+                          self.x + self.width, 
+                          self.y + self.height/2 - 4)
+        self.room.add_room_object(new_laser)
+        self.can_shoot = False
+        self.set_timer(10,self.reset_shot)
+            
+    def reset_shot(self):
+        """
+        Allows ship to shoot again
+        """
+        self.can_shoot = True
--- Objects/__init__.py
+++ Objects/__init__.py
@@ -2,3 +2,4 @@ from Objects.Title import Title
 from Objects.Ship import Ship
 from Objects.Boss import Boss
 from Objects.Rock import Rock
+from Objects.Laser import Laser
