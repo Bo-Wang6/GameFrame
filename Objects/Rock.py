3601ab6d22d205d77ccb3ed4fbf8ae7d523b3b17
Bo Wang <bowang355@gmail.com>
2026-06-01 15:11:14 +1000

    Ship Rock collision

--- Objects/Rock.py
+++ Objects/Rock.py
@@ -21,6 +21,9 @@ class Rock(RoomObject):
         angle = random.randint(135,225)
         self.set_direction(angle, 10)
 
+        # register events
+        self.register_collision_object("Ship")
+
     def step(self):
         """
         Determines what happens to the Rock on each tick of the game clock
@@ -37,3 +40,11 @@ class Rock(RoomObject):
         elif self.y > Globals.SCREEN_HEIGHT - self.height:
             self.y = Globals.SCREEN_HEIGHT - self.height
             self.y_speed *= -1
+
+    def handle_collision(self, other, other_type):
+        """
+        Handles the collision events for the Rock
+        """
+        
+        if other_type == "Ship":
+            self.room.running = False
